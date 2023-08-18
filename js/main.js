71681edf67aebfe16cb46af7476e7a5ea33cf5db
AixaHernandezv <aixahernandezv@gmail.com>
2023-08-17 21:10:32 -0400

    seleccionar-eliminar tarea

--- js/main.js
+++ js/main.js
@@ -2,51 +2,66 @@ const inputTask = document.querySelector("#inputTask");
 const btnAddTask = document.querySelector("#btnAddTask");
 const todoList = document.querySelector("#todoList tbody");
 let tableTask = [];
+
+
 inputTask.focus();
 btnAddTask.addEventListener("click", addTarea);
 
-
-
 function addTarea() {
-    
-    if (inputTask.value === '') {
-        alert('no hay nada que agegar');
+    if (inputTask.value === "") {
+        alert("no hay nada que agegar");
         inputTask.focus();
-    }
-    else {
-       
+    } else {
         tableTask.push(inputTask.value);
         listTasks();
-        inputTask.value = '';
+        inputTask.value = "";
         inputTask.focus();
     }
 }
 
 function listTasks() {
-    todoList.innerHTML = '';
+    todoList.innerHTML = "";
     tableTask.forEach((task, index) => {
         const row = document.createElement("tr");
         row.innerHTML = `
-        <td>${task}</td>
-        <td>  <button id="btnDelete${index}" type="button" data-id="${index}">x</button> </td>
+            <td id="taskId${index}" data-id="${index}">
+                ${task}
+            </td>
+            <td> 
+                <button id="btnDelete${index}" type="button" data-id="${index}">
+                    X
+                </button>
+            </td>
         `;
-        row.setAttribute('id',`row${index}`);
+
+        row.setAttribute("id", `row${index}`);
         todoList.appendChild(row);
+
         const btnDelete = document.querySelector(`#btnDelete${index}`);
-        btnDelete.addEventListener('click', deleteTask)
+        btnDelete.addEventListener("click", deleteTask);
+        const taskSelected = document.querySelector(`#taskId${index}`);
+        taskSelected.addEventListener("click", selectTask);
     });
+    console.log(tableTask);
 }
 
 function deleteTask(e) {
-
-   const index = e.target.getAttribute("data-id");
-    tableTask.splice(index,1);
+    const index = e.target.getAttribute("data-id");
+    tableTask.splice(index, 1);
     const rowToDelete = document.querySelector(`#row${index}`);
     todoList.removeChild(rowToDelete);
-    
+}
 
+function selectTask(e) {
+    deselectAllTodo();
+    const index = e.target.getAttribute("data-id");
+    const rowSelected = document.querySelector(`#row${index}`);
+    rowSelected.classList.add('selected-row');
+}
 
-
-
-    
+function deselectAllTodo() {
+    tableTask.forEach((task,i) => {
+        const row = document.querySelector(`#row${i}`);
+        row.classList.remove('selected-row');
+    });
 }
